d5c9ce6c759af4fc49d473b8cd4e34a3543e24d4
Jayanta Debnath <133887991+Creative-Wave@users.noreply.github.com>
2025-05-08 23:20:24 +0530

    Add files via upload

---
+++ src/components/Blur.tsx
@@ -0,0 +1,9 @@
+const Blur = () => {
+  return (
+    <>
+      <div className="h-auto relative top-0 left-0 -z-30"></div>
+    </>
+  );
+};
+
+export default Blur;
---
+++ src/components/Keyskill.tsx
@@ -0,0 +1,46 @@
+const Keyskill = () => {
+  return (
+    <>
+      <div className="h-auto bg-cover bg-center px-5 md:px-20 ">
+        {/* Gradient overlay */}
+        <div className="inset-0 h-full flex flex-col justify-center items-center md:px-20 px-10 py-10">
+          <div className="flex flex-col justify-center items-center space-y-8">
+            <div className="grid grid-cols-4 md:grid-cols-8 gap-8">
+              {/* Responsive Icons */}
+              {[
+                "icon1.png",
+                "icon2.png",
+                "icon3.png",
+                "icon4.png",
+                "icon5.png",
+                "icon6.png",
+                "icon7.png",
+                "icon8.png",
+                "icon9.png",
+                "icon10.png",
+                "icon11.png",
+                "icon12.png",
+                "icon13.png",
+                "icon14.png",
+                "icon15.png",
+                "icon16.png",
+              ].map((icon, index) => (
+                <div
+                  key={index}
+                  className="w-16 h-16 sm:w-20 sm:h-20 lg:w-[110px] lg:h-[110px] border-2 border-[#FF8000] rounded-xl md:rounded-3xl flex justify-center items-center hover:shadow-lg hover:shadow-[#FF8000] cursor-pointer transition-shadow duration-300 ease-in-out"
+                >
+                  <img
+                    src={icon}
+                    className="w-2/4 h-2/4 bg-black rounded-full object-cover"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Keyskill;
